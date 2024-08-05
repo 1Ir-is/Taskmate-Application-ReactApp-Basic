9cd5e46be84111255b3830a27427c7321522b5b8
1Ir-is <mhuynk1005@gmail.com>
2024-08-05 16:40:51 +0700

    feat: add validate for empty input

--- src/components/AddTask.js
+++ src/components/AddTask.js
@@ -1,36 +1,49 @@
-import React from 'react'
+import React from 'react';
 
 export const AddTask = ({ taskList, setTasklist, task, setTask }) => {
+  const handleSubmit = (event) => {
+    event.preventDefault();
 
-    const handleSubmit = (event) => {
-        event.preventDefault();
-
-        if (task.id) {
-            const date = new Date();
-            const updatedTaskList = taskList.map((todo) => (
-                todo.id === task.id ? {id: task.id, name: task.name, time: `${date.toLocaleTimeString()} ${date.toLocaleDateString()}`} : todo
-            ));
-            setTasklist(updatedTaskList);
-            setTask({});
-        } else {
-            const date = new Date();
-            const newTask = {
-                id: date.getTime(), 
-                name: event.target.task.value, 
-                time: `${date.toLocaleTimeString()} ${date.toLocaleDateString()}`
-            };
-    
-            setTasklist([...taskList, newTask]);
-            setTask({});
-        }
+    if (!task.name || task.name.trim() === "") {
+      alert("Task name cannot be empty");
+      return;
     }
+
+    const date = new Date();
+    if (task.id) {
+      const updatedTaskList = taskList.map((todo) =>
+        todo.id === task.id
+          ? { id: task.id, name: task.name, time: `${date.toLocaleTimeString()} ${date.toLocaleDateString()}` }
+          : todo
+      );
+      setTasklist(updatedTaskList);
+      setTask({});
+    } else {
+      const newTask = {
+        id: date.getTime(),
+        name: event.target.task.value,
+        time: `${date.toLocaleTimeString()} ${date.toLocaleDateString()}`,
+      };
+
+      setTasklist([...taskList, newTask]);
+      setTask({});
+    }
+  };
 
   return (
     <section className='addTask'>
-        <form onSubmit={handleSubmit}>
-            <input type="text" name="task" value={task.name || ""} autoComplete='off' placeholder='Add Task' maxLength="25" onChange={e => setTask({ ...task, name: e.target.value })}/>
-            <button type='submit'>{ task.id ? "Update" : "Add" }</button>
-        </form>
+      <form onSubmit={handleSubmit}>
+        <input
+          type="text"
+          name="task"
+          value={task.name || ""}
+          autoComplete='off'
+          placeholder='Add Task'
+          maxLength="25"
+          onChange={(e) => setTask({ ...task, name: e.target.value })}
+        />
+        <button type='submit'>{task.id ? "Update" : "Add"}</button>
+      </form>
     </section>
-  )
-}
+  );
+};
